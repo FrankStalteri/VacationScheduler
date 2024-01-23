82604045febddfcf6ae6c8981db4dc306c4ee746
Frank <strikezone51@gmail.com>
2024-01-23 15:43:03 -0500

    Initial commit

---
+++ app/src/main/java/com/example/vacationapp/UI/VacationList.java
@@ -0,0 +1,16 @@
+package com.example.vacationapp.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.vacationapp.R;
+
+public class VacationList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vacation_list);
+    }
+}
